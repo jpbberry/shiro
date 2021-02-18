647528ce66540edd4b23b6220c9dba95b31d1569
jpbberry <jb@jt3ch.net>
2021-02-17 23:52:05 -0500

    add help command

--- src/worker.js
+++ src/worker.js
@@ -15,10 +15,29 @@ worker.commands
     const image = await shiro[ctx.command.nsfw ? 'nsfw' : 'sfw'](ctx.command.command)
 
     ctx.embed
+      .color(0xFCD5BA)
       .image(image)
       .send()
 
     return false
+  })
+  .add({
+    command: 'help',
+    exec: (ctx) => {
+      const embed = ctx.embed
+        .color(0xFCD5BA)
+        .description('https://shiro.gg')
+        .title('Shiro Help')
+        .timestamp()
+
+      const sfwCommands = worker.commands.commands.filter(x => !x.nsfw)
+      embed.field('SFW Commands', sfwCommands.map(x => `- \`${ctx.prefix} ${x.command}\``).join('\n'))
+
+      const nsfwCommands = worker.commands.commands.filter(x => x.nsfw)
+      embed.field('NSFW Commands', nsfwCommands.map(x => `- \`${ctx.prefix} ${x.command}\``).join('\n'))
+
+      embed.send()
+    }
   })
 
 sfw.forEach(type => {
