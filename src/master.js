c6a7da106c8e3548007a02d9bda5798c7a51b824
jpbberry <jb@jt3ch.net>
2021-02-17 23:34:42 -0500

    readd nsfw caching

--- src/master.js
+++ src/master.js
@@ -4,14 +4,14 @@ const config = require('../config.json')
 const master = new Master(require('path').resolve(__dirname, './worker.js'), {
   token: config.token,
   shardsPerCluster: 9,
-  cache: { // disable all defaults except for guilds
+  cache: { // disable all defaults except for guilds anmd channels
     roles: false,
-    channels: false,
     self: false
   },
   intents: ['GUILDS', 'GUILD_MESSAGES'],
   cacheControl: {
-    guilds: [] // only need ID for guild count, no props
+    guilds: [], // only need ID for guild count, no props
+    channels: ['nsfw'] // only needed for nsfw commands
   }
 })
 
